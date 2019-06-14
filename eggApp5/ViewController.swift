3e31f5f33f8a0c4752169588de9de8d82e3c25c6
damp0010 <end.of.the.road.10@gmail.com>
2019-06-14 17:55:54 +0900

    名前はeggAppだけど中身はPushUpApp

--- eggApp5/ViewController.swift
+++ eggApp5/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+    countLabel.text = String(count)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var count:Int = 0
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var textLabel: UILabel!
 
-
+    
+    
+    
+    @IBOutlet weak var eggButton: UIButton!
+    @IBAction func eggButton(_ sender: Any) {
+    count += 1
+    countLabel.text = String(count)
+        if count == 10{
+            textLabel.text = "この回数でわざわざ画面見るな！"
+        
+        }
+        
+        else if count == 20 {
+            textLabel.text = "画面見る暇があったら、筋肉と会話しろ！"
+            
+        }
+            
+        else if count == 30 {
+            textLabel.text = "フォーム崩れているぞ！筋肉に失礼だ！"
+            
+        }
+            
+        else if count == 40 {
+            textLabel.text = "馬鹿野郎！ちゃんと負荷かけろ！"
+        
+        }
+        else if count == 50 {
+            textLabel.text = "お疲れさん！次のセットいくぞ！"
+            
+        }
+        else if count == 51 {
+            countLabel.text = ""
+            textLabel.text = "準備できたか？？"
+            
+        }
+        else if count >= 52 {
+            countLabel.text = ""
+            textLabel.text = "早くしろ！！"
+            
+        }
+    
+    }
+    
+    
+    
+    
 }
 
